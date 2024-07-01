dd4253966c833c3e844598685551a48123182eb2
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-07-01 11:01:45 -0400

    Update PreferIPv4overIPv6.ps1
    Added notes

--- PreferIPv4overIPv6.ps1
+++ PreferIPv4overIPv6.ps1
@@ -1,15 +1,26 @@
 <#
-Sets device to prefer IPv4 over IPv6
-By default Windows trust and prefers IPv6 over IPv4, while if you are using IPv6 that may be great but for most of the world it can lead to issues. 
-Since IPv6 information is ditrubuted automatically it easy to plant a black box that gives out IPv6 information such as DNS and hijack the DNS in an enviroment since it will be preferred to IPv4 settings you are handing out 
+    Sets device to prefer IPv4 over IPv6
+
+    Why prefer IPv4 over IPv6 (in most cases)?
+        While IPv6 is the future and necessary due to the exhaustion of IPv4 addresses externally, most internal networks still predominantly use IPv4.
+        Security Concerns
+            IPv6's automatic configuration mechanisms can be exploited in certain scenarios to hijack DNS or perform other attacks.
+        Compatibility Issues
+            Some older devices or software might not work correctly with IPv6, leading to unexpected problems.
+
+    Note
+        IPv6-Only Networks: If you are in an environment that exclusively uses IPv6, DO NOT make this change, as it will break connectivity.
 #>
 
-$IPVersionPreference.DisabledComponents = 0
+#Sets a default value in case the registry key doesn't exist yet.
+$IPv6DisabledComponents = 0 
 
-$IPVersionPreference = Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name DisabledComponents
+#Get the current IPv6 preference configuration from the registry
+$IPv6DisabledComponents = Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name DisabledComponents -ErrorAction SilentlyContinue
 
 
-if($IPVersionPreference.DisabledComponents -ne 32) {
-    #Sets the computer to prefer IPv4 over IPv6
-    New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name DisabledComponents -Value 32 -PropertyType DWORD  -Force
+#Check if IPv6 is not already preferred (value is not 32)
+if($IPv6DisabledComponents -ne 32) {
+    #Sets the DisabledComponents value to 32, which makes the computer prefer IPv4 over IPv6
+    New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name DisabledComponents -Value 32 -PropertyType DWORD -Force
 }
